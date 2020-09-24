b84fa68639596308e9719483c3442f53da98eca8
remyatoney <71757499+remyatoney@users.noreply.github.com>
2020-09-24 19:50:54 +0530

    Update navbar.inc.php

--- includes/navbar.inc.php
+++ includes/navbar.inc.php
@@ -20,23 +20,6 @@
     <link rel="stylesheet" href="/test/css/bootstrap-social.css">
 </head>
 <body>
-<nav aria-label="Page navigation example">
-  <ul class="pagination">
-    <li class="page-item">
-      <a class="page-link" href="#" aria-label="Previous">
-        <span aria-hidden="true">&laquo;</span>
-      </a>
-    </li>
-    <li class="page-item"><a class="page-link" href="#">1</a></li>
-    <li class="page-item"><a class="page-link" href="#">2</a></li>
-    <li class="page-item"><a class="page-link" href="#">3</a></li>
-    <li class="page-item">
-      <a class="page-link" href="#" aria-label="Next">
-        <span aria-hidden="true">&raquo;</span>
-      </a>
-    </li>
-  </ul>
-</nav>
 <nav class="navbar navbar-dark navbar-expand-sm fixed-top" style="background-color: black;">
         <div class="container">
            <div class="collapse navbar-collapse" id="Navbar">
